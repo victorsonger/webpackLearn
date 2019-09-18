bbc155f7553c94a96961a59381edb4c209d88f46
yunfeng.chen <yunfeng.chen@luckincoffee.com>
2019-09-18 11:31:54 +0800

    回退处理

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,24 +1,9 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.jpg";
-import Data from "./data.xml";
 
 function component() {
   var element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack", " "]);
-  element.classList.add("hello");
-
-  var myIcon = new Image();
-  console.log("myIcon", myIcon);
-  myIcon.src = Icon;
-  myIcon.classList.add("what");
-
-  element.appendChild(myIcon);
-
-  console.log("myIcon2", myIcon);
-
-  console.log('Data', Data);
   
   return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -6,24 +6,4 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ["csv-loader"]
-      },
-      {
-        test: /\.xml$/,
-        use: ["xml-loader"]
-      }
-    ]
-  }
 };
