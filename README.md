b51f5157421035333c3c7c741616aeabb22a9e62
yunfeng.chen <yunfeng.chen@luckincoffee.com>
2019-09-20 14:51:14 +0800

    回退到 “起步”代码状态

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ https://www.notion.so/webpack-c51ad3910ea3445b8134aa03b0070b85
 ## webpack 指南：
 https://www.webpackjs.com/guides/
 
-## 模块热替换
+## tree shaking
+代码沿用了 起步  的代码
 https://webpack.docschina.org/guides/hot-module-replacement/
 
--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,35 +1,11 @@
 import _ from "lodash";
-import printMe from "./print.js";
-import './style.css';
 
 function component() {
-  var element = document.createElement("div");
-  var btn = document.createElement("button");
-  console.log("btn", btn);
-  btn.name = "imabtn";
-  console.log("namedbtn", btn);
+  let element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack", " "]);
-
-  btn.innerHTML = "点击以console";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   return element;
 }
 
-// document.body.appendChild(component());
-let element = component(); // 存储element， 以在print.js修改时重新是
-document.body.appendChild(element);
-
-console.log("module.hot.accept", module.hot.accept);
-if (module.hot) {
-  module.hot.accept("./print.js", function() {
-    console.log("正在接收更新了的printMe模块");
-    // printMe();
-    document.body.removeChild(element);
-    element = component(); // 重新渲染“component”， 以便更新 click 事件处理函数
-    document.body.appendChild(element);
-  });
-}
+document.body.appendChild(component());
