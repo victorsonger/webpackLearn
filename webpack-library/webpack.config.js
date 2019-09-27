f02cce1596807fc5d191f761699da5ab7a5bf051
yunfeng.chen <yunfeng.chen@luckincoffee.com>
2019-09-27 16:22:03 +0800

    将模块配置为一个library

--- webpack-library/webpack.config.js
+++ webpack-library/webpack.config.js
@@ -4,7 +4,11 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "webpack-number.js"
+    filename: "webpack-number.js",
+    // 将你的 library bundle 暴露为名为 webpackNumbers 的全局变量，consumer 通过此名称来 import
+    library: 'webpackNumbers',
+    // 为了让 library 和其他环境兼容，则需要在配置中添加 libraryTarget 属性。这个选项可以控制以不同形式暴露 library
+    libraryTarget: 'umd'
   },
   // 外部扩展
   externals: {
