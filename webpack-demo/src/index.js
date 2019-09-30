0c80ac51b0c148cebc46a74c6e3e28ca121f32f4
yunfeng.chen <yunfeng.chen@luckincoffee.com>
2019-09-30 09:05:12 +0800

    shimming-
    抽离出某个包中的某个具体方法

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,11 @@
-import _ from "lodash";
+// 出于演示目的，例如把这个应用程序中的模块依赖，改为一个全局变量依赖。要实现这些，我们需要使用 ProvidePlugin 插件
+// import _ from "lodash";
 
 function component() {
   let element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  // element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = join(["Hello", "webpack"], " ");
 
   return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: require.resolve(index.js),
+        use: ''
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+    // 我们本质上所做的，就是告诉 webpack……  如果你遇到了至少一处用到 _ 变量的模块实例，那请你将 lodash package 引入进来，并将其提供给需要用到它的模块。
+    //  _: 'lodash'
+    // 数组路径  无论 join 方法在何处调用，我们都只会获取到 lodash 中提供的 join 方法
+    join: ['lodash', 'join']
+    })
+  ]
 };
